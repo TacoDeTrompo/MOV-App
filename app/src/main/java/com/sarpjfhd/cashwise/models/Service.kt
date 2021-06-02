86a62a9d01a9493ae4913313121a0cdc9ea21dfc
TacoDeTrompo <rodrguez.samuel@gmail.com>
2021-06-01 19:44:48 -0500

    updated service endpoints

--- app/src/main/java/com/sarpjfhd/cashwise/models/Service.kt
+++ app/src/main/java/com/sarpjfhd/cashwise/models/Service.kt
@@ -3,13 +3,38 @@ import retrofit2.Call
 import retrofit2.http.*
 
 interface Service {
-    @GET("Profiles/{id}")
-    fun getProfile(@Path("id") id: Int): Call<List<Profile>>
+    @GET("profiles/{id}")
+    fun getProfile(@Path("id") id: Int): Call<Profile>
 
-    @GET("Profiles")
+    @GET("profiles")
     fun getProfiles(): Call<List<Profile>>
 
+    @GET("expenses/{id}")
+    fun getExpenses(@Path("id") id: Int): Call<Expense>
+
+    @GET("ingress/{id}")
+    fun getIngress(@Path("id") id: Int): Call<Ingress>
+
+    @GET("expenses")
+    fun getExpenses(): Call<List<Expense>>
+
+    @GET("ingress")
+    fun getIngresses(): Call<List<Ingress>>
+
+    @GET("userdata/{id}")
+    fun getUserData(@Path("id") id: Int): Call<User>
+
+    @GET("advice")
+    fun getAdvice(): Call<List<String>>
+
+    @GET("advice/{id}")
+    fun getAdvices(@Path("id") id: Int): Call<String>
+
     @Headers("Content-Type: application/json")
-    @POST("LogIn")
-    fun logIn(@Body userData: User): Call<Int>
+    @POST("checkToken")
+    fun checkToken(@Body token: String): Call<Boolean>
+
+    @Headers("Content-Type: application/json")
+    @POST("login")
+    fun logIn(@Body userData: User): Call<String>
 }
